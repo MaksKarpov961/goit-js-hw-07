ed181e1f1228d7df219197da6a08474209beb7e6
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-08-08 13:14:16 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const buttonChange = document.querySelector(".change-color")
+const output = document.querySelector(".color")
+const bodyColor = document.querySelector("body")
+
+
+buttonChange.addEventListener("click", buttonChangeClick)
+
+
+function buttonChangeClick(event) {
+  output.textContent = getRandomHexColor()
+  bodyColor.style.backgroundColor = getRandomHexColor()
+  
+}
